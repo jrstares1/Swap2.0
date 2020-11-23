755f419b160ca41fa9b8667f781e9bc609f7120b
Mitch Frauenheim <mitch.frauenheim@wustl.edu>
2020-11-23 13:01:00 -0600

    Progress on alert

--- Swap2/ViewControllers/SignUpViewController.swift
+++ Swap2/ViewControllers/SignUpViewController.swift
@@ -166,6 +166,7 @@ class SignUpViewController: UIViewController {
                             }
             
                         }
+                        self.transitionToHome()
                     }else{
                         print("USER NULL")
                     }
