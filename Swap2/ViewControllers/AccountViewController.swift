02184a41623093fc9ad1f5c65fe07c648b0a08a6
Justin Stares <jrstares@wustl.edu>
2020-11-28 14:37:47 -0500

    delete user done

--- Swap2/ViewControllers/AccountViewController.swift
+++ Swap2/ViewControllers/AccountViewController.swift
@@ -116,13 +116,45 @@ class AccountViewController: UIViewController{
     }
     
     
-    @IBAction func SignOut(_ sender: Any) {
-        self.userDefault.set(false, forKey: "usersignedin")
-        self.userDefault.synchronize()
+    @IBAction func deleteProfile(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Alert", message: "Are you sure you want to delete your account?", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.destructive, handler: { action in
+        
+        
+            let user = Auth.auth().currentUser
+            user?.delete(completion: {error in
+                if error != nil{
+                    print("account deletion error")
+                }else{
+                
+                    print("account deleted")
+                    self.userDefault.set(false, forKey: "usersignedin")
+                    self.userDefault.synchronize()
+                    
+                    let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as UIViewController
+                    self.present(initViewController, animated: true, completion: nil)
+                }
+            })
+                
+        }))
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+       
+    }
+    
+    
+    
+    @IBAction func SignOut(_ sender: Any) {
+
         let firebaseAuth = Auth.auth()
         do {
           try firebaseAuth.signOut()
+            self.userDefault.set(false, forKey: "usersignedin")
+            self.userDefault.synchronize()
+            
             let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as UIViewController
             self.present(initViewController, animated: true, completion: nil)
