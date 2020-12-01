2dd0a3363087db422326a69cdcde362fef3be781
Gillian <gdlaming3@gmail.com>
2020-12-01 10:43:42 -0600

    popup functions working

--- Swap2/ViewControllers/PopUpViewController.swift
+++ Swap2/ViewControllers/PopUpViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class PopUpViewController: UIViewController {
-
+    
+    let userDefault = UserDefaults.standard
+    let launchedBefore = UserDefaults.standard.bool(forKey: "usersignedin")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
@@ -42,14 +47,48 @@ class PopUpViewController: UIViewController {
                 }
         });
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func deleteAccount(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Are you sure you want to delete your account?", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.destructive, handler: { action in
+            let user = Auth.auth().currentUser
+            user?.delete(completion: {error in
+                if error != nil{
+                    print("account deletion error")
+                }
+                else{
+                    self.userDefault.set(false, forKey: "usersignedin")
+                    self.userDefault.synchronize()
+                    let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as UIViewController
+                    self.present(initViewController, animated: true, completion: nil)
+                }
+            })
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func signOut(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            self.userDefault.set(false, forKey: "usersignedin")
+            self.userDefault.synchronize()
+            
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as UIViewController
+            self.present(initViewController, animated: true, completion: nil)
+        }
+        catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    @IBAction func help(_ sender: Any) {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Onboarding", bundle: nil)
+        let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "OnBoardingVC") as! OnBoardingViewController
+        initViewController.modalPresentationStyle = .fullScreen
+        self.present(initViewController, animated: true, completion: nil)
+    }
 }
